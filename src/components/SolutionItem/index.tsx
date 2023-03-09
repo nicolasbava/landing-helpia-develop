8c50da562fb98c7b5b0f5d7a7ed54a7605a95529
xdariel <dariel87@gmail.com>
2023-03-09 08:33:48 -0500

    Check Point

---
+++ src/components/SolutionItem/index.tsx
@@ -0,0 +1,39 @@
+import  {FC} from "react"
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import React from "react";
+import {styled, Typography} from "@mui/material";
+
+
+
+
+const StyledImage = styled("img")(() => ({
+    width: "auto",
+    height: "150px",
+}))
+
+type SolutionItemProps = {
+    title: string
+    text: string
+
+    src: string
+}
+const SolutionItem:FC<SolutionItemProps> = ({src, title, text})=> {
+
+
+    return (<Stack alignItems={"center"}>
+        <StyledImage
+            src={src}
+            alt={title}
+            loading="lazy"
+
+        />
+
+        <Typography sx={{fontWeight: "bold", color: "primary.light", my:2}}>{title}</Typography>
+        <Box sx={{width: {xs:"100%", sm: "80%", lg: "75%"}, textAlign: "center"}}>
+            <Typography >{text}</Typography>
+        </Box>
+    </Stack>)
+}
+
+export default SolutionItem
