cdad810f5edb47307476ba7c063a749ae5508592
nicolasbava <nicobava96@gmail.com>
2023-03-14 11:20:55 -0300

    fix

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -29,7 +29,7 @@ const Footer = forwardRef<any, BoxProps>((props, ref) => {
                             <Link href='/'>
                                 <Logo alt={"footer-logo"}  width={178} height={80}/>
                             </Link>
-                            <Typography variant="caption" sx={{maxWidth: '350px'}}>Somos especialistas en atención al cliented.
+                            <Typography variant="caption" sx={{maxWidth: '350px'}}>Somos especialistas en atención al cliente.
                                 Conócenos.</Typography>
                         </StyledFooterSection>
                     </Grid>
